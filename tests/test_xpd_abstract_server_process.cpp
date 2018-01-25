fb3b7a0abfc77adaf5b1ab1ec589afd27b19049e
Alex Nadzharov <anadjarov@gmail.com>
2018-01-25 17:20:00 +0300

    PdArguments tests

--- tests/test_xpd_abstract_server_process.cpp
+++ tests/test_xpd_abstract_server_process.cpp
@@ -15,13 +15,13 @@ extern const char* CATCHER_NAME;
 class AbstractServerProcessMock : public AbstractServerProcess {
 public:
     AbstractServerProcessMock(const AbstractServer* parent, const ServerProcessSettings& s)
-        : AbstractServerProcess(parent, s) {};
+        : AbstractServerProcess(parent, s){};
 
     virtual void dspSwitch(bool value) override{};
-    virtual CanvasPtr createCanvas() override {return CanvasPtr(0);}
+    virtual CanvasPtr createCanvas() override { return CanvasPtr(0); }
 
-    virtual bool loadLibrary(const std::string& libraryName) override { return true;};
-    virtual bool loadExternal(const std::string& externalName) override { return true;};
+    virtual bool loadLibrary(const std::string& libraryName) override { return true; };
+    virtual bool loadExternal(const std::string& externalName) override { return true; };
 };
 
 TEST_CASE("AbstractServerProcess", "[AbstractServerProcess]")
@@ -31,13 +31,14 @@ TEST_CASE("AbstractServerProcess", "[AbstractServerProcess]")
 
     SECTION("abstractServerProcess")
     {
-//        ServerProcessSettings ts = srv.settings();
-//        REQUIRE(ts == s);
 
-//        ServerProcessSettings s2;
-//        srv.setSettings(s2);
-//        ts = srv.settings();
-//        REQUIRE(ts == s2);
+        //        ServerProcessSettings ts = srv.settings();
+        //        REQUIRE(ts == s);
+
+        //        ServerProcessSettings s2;
+        //        srv.setSettings(s2);
+        //        ts = srv.settings();
+        //        REQUIRE(ts == s2);
 
         //
         REQUIRE(srv.loadLibrary(""));
@@ -45,29 +46,4 @@ TEST_CASE("AbstractServerProcess", "[AbstractServerProcess]")
 
         REQUIRE(srv.createCanvas().get() == 0);
     }
-
-    //    SECTION("serverSettings")
-    //    {
-    //        ServerSettings s1("testOne");
-    //        ServerSettings s2("testOne");
-    //        ServerSettings s3("testThree");
-
-    //        REQUIRE(s1.name() == "testOne");
-
-    //        REQUIRE(s1 == s2);
-    //        REQUIRE(s1 != s3);
-    //    }
-
-    //    SECTION("abstractServer")
-    //    {
-    //        ProcessPtr p = srv.createProcess();
-    //        REQUIRE(p.get() == 0);
-
-    //        REQUIRE(srv.name() == "local");
-    //        REQUIRE(srv.settings() == ServerSettings("local"));
-
-    //        srv.setSettings(ServerSettings("newLocal"));
-    //        REQUIRE(srv.name() == "newLocal");
-    //        REQUIRE(srv.settings() == ServerSettings("newLocal"));
-    //    }
 }
